4164cc6c442724ef7128eda53e8d2365cc80aa06
Gabriel Machado <gabriel.alcantaramachadoz@gmail.com>
2023-11-28 18:44:10 -0300

    estilização do layout

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,48 @@
-
+import { useState, FormEvent } from "react"
+import {User} from "../types/User"
 
 const Form = () => {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [agree, setAgree] = useState(false)
+
+  const[error, setErrors] = useState<User | null>(null)
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault()
+    alert("Teste")
+  }
   return (
-    <div>Form</div>
+    <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
+      <div className="flex flex-col">
+        <label className="text-sm" htmlFor="name">
+          Nome
+        </label>
+        <input type="text" placeholder="Digite o seu nome" className="rounded-lg py-2 px-2 text-sm placeholder:text-sm placeholder:text-stone-400"/>
+      </div>
+
+      <div className="flex flex-col">
+        <label className="text-sm" htmlFor="email">
+          E-mail
+        </label>
+        <input type="email" placeholder="Insira seu melhor e-mail" className="rounded-lg py-2 px-2 text-sm placeholder:text-sm placeholder:text-stone-400"/>
+      </div>
+
+      <div className="flex flex-col">
+        <a href="#" className="text-xs underline mb-2">
+          Leia os termos
+        </a>
+        <div className="flex gap-2 items-center">
+          <input type="checkbox" placeholder="Digite o seu nome"/>
+          <label className="text-sm" htmlFor="agree">
+            Concordo com os termos
+          </label>
+        </div>
+      </div>
+      <button type="submit" className="bg-slate-600 hover:bg-slate-500 font-medium text-sm py-2 px-4 rounded-lg text-white">
+        Cadastrar
+      </button>
+    </form>
   )
 }
 
